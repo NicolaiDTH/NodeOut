07f2090eb57e6f22b262b25cc0f1c05c7cd3b8b2
Nicolai <nicolai@mmg.io>
2017-01-23 18:51:27 +0000

    Finalised the routes and responses

--- router.js
+++ router.js
@@ -1,22 +1,28 @@
 // Handle HTTP route GET / and POST / i.e. Home
 var Profile = require("./profile.js");
 var renderer = require("./renderer.js");
+var querystring = require("querystring");
+var commonHeaders = {'Content-Type': 'text/html'};
 
 function home(request, response) {
 
   // if url == "//" && GET
  if (request.url === "/") {
+  if (request.method.toLowerCase() === "get") {
     // Show search field
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  renderer.view("header", {}, response);
-  renderer.view("search", {}, response);
-  renderer.view("footer", {}, response);
-  response.end();
+    response.writeHead(200, commonHeaders);
+    renderer.view("header", {}, response);
+    renderer.view("search", {}, response);
+    renderer.view("footer", {}, response);
+    response.end();
 
-
-  // if url == "/" && post
-    // redirect to /:username
-
+  } else {
+    request.on("data", function(postBody){
+      var query = querystring.parse(postBody.toString());
+      response.writeHead(303, {"Location": "/" + query.username});
+      response.end();
+   });
+  }
  }
 }
 
@@ -27,14 +33,14 @@ function user(request, response) {
   // if url == "/....."
  var username = request.url.replace("/", "");
  if (username.length > 0) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.writeHead(200, commonHeaders);
   renderer.view("header", {}, response);
 
     // get JSON from TTH
   var studentProfile = new Profile(username);
 
    // on "end"
-  studentProfile.on("end", function(profileJSON){
+  studentProfile.on("end", function(profileJSON) {
   // show profile
 
 
@@ -52,7 +58,7 @@ function user(request, response) {
   });
 
   // on "error"
-  studentProfile.on("error", function(error){
+  studentProfile.on("error", function(error) {
    //show error
   renderer.view("error", {errorMessage: error.message}, response);
   renderer.view("search", {}, response);
