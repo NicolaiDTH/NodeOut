725c6ffff0b37c331f5f00634abf38a3435c6cd3
Nicolai <nicolai@mmg.io>
2017-01-23 14:48:10 +0000

    Replacing values with content from TTH

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 // Handle HTTP route GET / and POST / i.e. Home
 var Profile = require("./profile.js");
+var renderer = require("./renderer.js");
 
 function home(request, response) {
 
@@ -7,9 +8,11 @@ function home(request, response) {
  if (request.url === "/") {
     // Show search field
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write("Header + \n");
-  response.write("Search + \n");
-  response.end("Footer + \n");
+  renderer.view("header", {}, response);
+  renderer.view("search", {}, response);
+  renderer.view("footer", {}, response);
+  response.end();
+
 
   // if url == "/" && post
     // redirect to /:username
@@ -25,7 +28,7 @@ function user(request, response) {
  var username = request.url.replace("/", "");
  if (username.length > 0) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write("Header + \n");
+  renderer.view("header", {}, response);
 
     // get JSON from TTH
   var studentProfile = new Profile(username);
@@ -43,15 +46,18 @@ function user(request, response) {
     javascriptPoints: profileJSON.points.JavaScript
     }
     // Simple response
-    response.write(values.username + " has " + values.badges + " badges \n");
-    response.end("Footer + \n");
+    renderer.view("profile", values, response);
+    renderer.view("footer", {}, response);
+    response.end();
   });
 
   // on "error"
   studentProfile.on("error", function(error){
    //show error
-  response.write(error.message + "\n");
-  response.end("Footer + \n");
+  renderer.view("error", {errorMessage: error.message}, response);
+  renderer.view("search", {}, response);
+  renderer.view("footer", {}, response);
+  response.end();
   });
  }
 }
